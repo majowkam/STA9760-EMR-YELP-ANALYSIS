90fc22c1e1af35f7dc11f6f5e6f7b7e40ffe9de7
EC2 Default User <ec2-user@ip-172-31-95-30.ec2.internal>
2020-04-25 23:04:48 +0000

    kaggle to s3 complete

--- script/kaggle-to-s3.py
+++ script/kaggle-to-s3.py
@@ -8,14 +8,17 @@ def main(dsname,s3bucket):
 		get_kaggle(dsname,fpath)
 	except Exception as e:
 		print(e,'Could not get Kaggle data',sep='\n')
+	else:
+		print('Kaggle download complete')
 	try:
 		to_s3(s3bucket,fpath)
 	except Exception as e:
 		print(e,'Could not upload to s3',sep='\n')
+	else:
+		print('S3 upload complete')
 	for f in os.listdir(fpath):
-		os.remove(f)
+		os.remove(fpath+'/'+f)
 	os.rmdir(fpath)
-	print('all done matt')
 
 def get_kaggle(dsname,fpath):
 	import kaggle.api as k
@@ -27,7 +30,7 @@ def to_s3(bucket,fpath):
 	import os
 	s3 = boto3.client('s3')
 	for f in os.listdir(fpath):
-		s3.upload_file(f,bucket,f)
+		s3.upload_file(fpath+'/'+f,bucket,f)
 
 if __name__ == "__main__":
 	args   = sys.argv[1:]

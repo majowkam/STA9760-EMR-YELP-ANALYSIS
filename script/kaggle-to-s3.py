bc8a6ca21f8483a49218081dfeb3f7ad49540347
EC2 Default User <ec2-user@ip-172-31-95-30.ec2.internal>
2020-04-25 21:52:32 +0000

    seems to be working

--- script/kaggle-to-s3.py
+++ script/kaggle-to-s3.py
@@ -1,14 +1,34 @@
-import kaggle
-import boto3
 import sys
-import os
 
+def main(dsname,s3bucket): 
+	import os
+	fpath = 'temp'
+	os.mkdir(fpath)
+	try:
+		get_kaggle(dsname,fpath)
+	except Exception as e:
+		print(e,'Could not get Kaggle data',sep='\n')
+	try:
+		to_s3(s3bucket,fpath)
+	except Exception as e:
+		print(e,'Could not upload to s3',sep='\n')
+	for f in os.listdir(fpath):
+		os.remove(f)
+	os.rmdir(fpath)
+	print('all done matt')
 
-def main(): 
-	print('hello matt')
+def get_kaggle(dsname,fpath):
+	import kaggle.api as k
+	k.authenticate()
+	k.dataset_download_files(dsname,path='temp/',unzip=True)
 
+def to_s3(bucket,fpath):
+	import boto3
+	import os
+	s3 = boto3.client('s3')
+	for f in os.listdir(fpath):
+		s3.upload_file(f,bucket,f)
 
 if __name__ == "__main__":
 	args   = sys.argv[1:]
-	apitoken = os.environ['APIKEY']
-	main()
+	main(args[0],args[1])
